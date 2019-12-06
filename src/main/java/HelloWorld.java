53411104f0c348804014acbb52d90b588a5f482a
lihle <lihlemenz@gmail.com>
2019-12-06 11:53:10 +0200

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        staticFiles.location("/public");
+
+        get("/hello", (req, res) -> {
+            Map<String, String> dataMap = new HashMap<>();
+            dataMap.put("name", "Lihle");
+            return new ModelAndView(dataMap, "hello.hbs");
+
+        }, new HandlebarsTemplateEngine());
+    }
+
+
+}
