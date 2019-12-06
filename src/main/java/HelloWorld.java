f7c1ca2aa48318e518df9669772e2368990107e0
lihle <lihlemenz@gmail.com>
2019-12-06 17:34:33 +0200

    added greeting messages

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -15,20 +15,58 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
+        HelloWorld h = new HelloWorld();
         staticFiles.location("/public");
+
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> {
             return new ModelAndView(new HashMap<>(), "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/hello", (req, res) -> {
+        post("/greet", (req, res) -> {
+            String name = req.queryParams("firstname");
+            String language = req.queryParams("language");
+
+            h.getName(name, language);
+
             Map<String, String> dataMap = new HashMap<>();
-            dataMap.put("name", "Lihle");
+         dataMap.put("name", h.getName(name, language));
             return new ModelAndView(dataMap, "hello.hbs");
 
         }, new HandlebarsTemplateEngine());
 
     }
+
+    Map<String, Integer> greetedMap  = new HashMap<>();
+    public String getName(String name, String language) {
+
+        String greeting = "Invalid language";
+
+        if(language.equals("eng")) {
+            greeting = "Good Morning, " + name;
+        } else if (language.equals("xhosa")) {
+            greeting = "Molo, " + name;
+        } else if (language.equals("afr")) {
+            greeting = "Goeiemore, " + name;
+        }
+//        if(language == null && name == "") {
+//            System.out.println("Invalid");
+//        }
+        return greeting;
+    }
+
+    void counter(String name) {
+        if (greetedMap.containsKey(name)) {
+            greetedMap.put(name, 1);
+        } else {
+            greetedMap.put(name, greetedMap.get(name) + 1);
+        }
+    }
+
+
+
+
+
 
 }
